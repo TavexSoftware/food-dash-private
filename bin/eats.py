2a0ba35a1066b46ca60204bedeb75ca7b8de0849
Max H <maxplaysbest@gmail.com>
2020-09-09 11:45:26 +1200

    Update eats.py

--- bin/eats.py
+++ bin/eats.py
@@ -1,6 +1,16 @@
+print("Starting... \nPlease Wait")
 import asyncio
 import random
 import json
 import os
+
+print("==============")
+print("Tavex Presents")
+print("  Food Dash")
+print("==============")
+print("    ")
+print("    ")
+print("Update Log")
+print("Updates Here Please")
 
 print("Program Run")
