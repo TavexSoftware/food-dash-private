9aa6d5a0c2675a17f9b4d0a18f9ad10d8c315fea
Max H <maxplaysbest@gmail.com>
2020-09-09 12:02:40 +1200

    Update eats.py

--- bin/eats.py
+++ bin/eats.py
@@ -3,14 +3,17 @@ import asyncio
 import random
 import json
 import os
+import time
 
 print("==============")
 print("Tavex Presents")
 print("  Food Dash")
 print("==============")
-print("    ")
-print("    ")
+print("By Max And Tavish")
+print("==============")
+print("==============")
 print("Update Log")
 print("Updates Here Please")
+time.sleep(5)
 
 print("Program Run")
